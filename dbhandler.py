9d9a662358e72cc63bd8a03f33a433079247d622
Sam Drew <[sam-drew@outlook.com]>
2016-11-24 22:33:33 +0000

    Added basic connection information

--- dbhandler.py
+++ dbhandler.py
@@ -1,1 +1,8 @@
 import pymysql
+
+connection = pymysql.connect(host = "host",
+                             user = "user",
+                             password = "password",
+                             db = "db",
+                             charset = "utf8mb4",
+                             cursorclass = pymysql.cursors.DictCursor)
