0c0db8f424e199f529cfccaa7ac517c734862b50
Sam Drew <[sam-drew@outlook.com]>
2016-11-28 11:57:54 +0000

    Added checkEmail function to dbhandler

--- dbhandler.py
+++ dbhandler.py
@@ -1,12 +1,15 @@
 import pymysql
 
 # Initialise the connection with the DB
-connection = pymysql.connect(host = "host",
-                             user = "user",
-                             password = "password",
-                             db = "db",
-                             charset = "utf8mb4",
-                             cursorclass = pymysql.cursors.DictCursor)
+try:
+    connection = pymysql.connect(host = "host",
+                                user = "user",
+                                password = "password",
+                                db = "db",
+                                charset = "utf8mb4",
+                                cursorclass = pymysql.cursors.DictCursor)
+except:
+    print("Connection error, check network status, credentials, & dependancies")
 
 # Function to add new user
 def addUser(email, name, password, salt):
@@ -15,13 +18,20 @@ def addUser(email, name, password, salt):
         with connection.cursor() as cursor:
             # Insert new record, ID is blank as is self incrementing
             sql = "INSERT INTO 'users' ('email', 'name', 'password', 'salt') VALUES (%s, %s, %s, %s)"
-            cursor.execute(sql, (email, name, password, salt))
+            cursor.execute((sql % (email, name, password, salt)))
         # Commit the changes made to the DB
         connection.commit()
 
     # Handle any errors on MySQL's part
     except MySQLError as e:
-        print("Error: %s. Error code is %s"%(e, e.args[0]))
+        return("Error: %s. Error code is %s" % (e, e.args[0]))
 
     finally:
         connection.close()
+
+# Function to check if the new sign-up's email is already in use
+def checkEmail(email):
+    try:
+        with connection.cursor() as cursor:
+            sql = ("SELECT 'email' FROM 'users' WHERE 'email' = %s")
+            return(cursor.execute(sql % (email,)))
